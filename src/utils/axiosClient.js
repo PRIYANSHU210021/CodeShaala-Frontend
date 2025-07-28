81000f9db50c830131fdeabae9e80b8d7f73b593
Priyanshu <priyanshu210021@gmail.com>
2025-07-28 13:47:15 +0530

    add baseUrl of deployed backend

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosClient = axios.create({
     // baseURL:'http://localhost:3000',
-    baseURL:'https://code-shaala-backend.vercel.app',
+    baseURL:'https://codeshaala-backend.vercel.app/',
     // baseURL: isLocalhost 
     //     ? "http://localhost:3000" 
     //     : "https://code-shaala-backend.vercel.app",
